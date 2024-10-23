a00bbea6a1c916de85222899d542436e4c9edf3b
simsononroad <d-github@kopaszhegy.hu>
2024-10-23 16:43:32 +0200

    kerdesek

---
+++ kerdesek.py
@@ -0,0 +1,173 @@
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+def start():
+    print("-----------------------------------------------")
+    print("| Legyen ön is milliomos                      |")
+    print("| Tájékoztató:                                |")
+    print("| - Mindig a, b, c vagy d-vel kell válaszolnia|")
+    print("| - A játékmenet során bármikor kiszállhat    |")
+    print("|      ha beír egy 's' betűt                  |")
+    print("|                                             |")
+    print("|       1.start                               |")
+    print("|       2.kilépés                             |")
+    print("-----------------------------------------------")
+    print("             |           ")
+
+def q1():
+    print("-------------|------------------------------------------------------------------------")
+    print("| Hogyan folytatódik ifj.Jojann Strauss ismert operettjének címe. Egy éj ...?        |")
+    print("|       a. Esküvő után                                                               |")
+    print("|       b. És más semmi                                                   (5.000Ft)  |")
+    print("|       c. Velencében                                                                |")
+    print("|       d. A Velencei tónál                                                          |")
+    print("--------------------------------------------------------------------------------------")
+    print("             |                                          ")
+
+def q2():
+    print("-------------|-----------------------------------------------------------------------------")
+    print("|Az alábbiak közül melyik egy műfaj sejtelmességét kifejező irodalmi stíluselem elnevezése|")
+    print("|       a. Ódai pompa                                                                     |")
+    print("|       b. Lírai szikra                                                  (10.000Ft)       |")
+    print("|       c. Balladai homály  x                                                             |")
+    print("|       d. Drámai ború                                                                    |")
+    print("-------------------------------------------------------------------------------------------")
+    print("             |                                          ")
+
+def q3():
+    print("-------------|-----------------------------------------------------------------------------")
+    print("|                     Melyik városban feszítették keresztre jézust?                       |")
+    print("|       a. Betlehem                                                                       |")
+    print("|       b. Jeruzsálem x                                                  (25.000Ft)       |")
+    print("|       c. Rómában                                                                        |")
+    print("|       d. Názáretben                                                                     |")
+    print("-------------------------------------------------------------------------------------------")
+    print("             |                                          ")
+
+def q4():
+    print("-------------|-----------------------------------------------------------------------------")
+    print("| Melyik cízi jármű játszott szerepet 1917-ben a pétervári bolsevik felkelés kitörésekor? |")
+    print("|       a. A Patyomkin páncélos                                                           |")
+    print("|       b. Az Auróra cirkáló x                                           (50.000Ft)       |")
+    print("|       c. A Lenin gőzhajó                                                                |")
+    print("|       d. A Pravada torpedóromboló                                                       |")
+    print("-------------------------------------------------------------------------------------------")
+    print("             |                                          ")
+
+def q5():
+    print("-------------|-----------------------------------------------------------------------------")
+    print("|            Ki volt Szemiramisz, akinek a nevéhez a híres függőkert kapcsolódik          |")
+    print("|       a. A Patyomkin páncélos                                                           |")
+    print(f"|       b. Az Auróra cirkáló x                                           ({bcolors.WARNING}100.000Ft{bcolors.ENDC})      |")
+    print("|       c. A Lenin gőzhajó                                                                |")
+    print("|       d. A Pravada torpedóromboló                                                       |")
+    print("-------------------------------------------------------------------------------------------")
+    print("             |                                          ")
+
+def q6():
+    print("-------------|-----------------------------------------------------------------------------")
+    print("|                            Kik jártak a Sóhajok hídján?                                 |")
+    print("|       a. A bírák                                                                        |")
+    print("|       b. A szűzleányok                                                (200.000Ft)       |")
+    print("|       c. Az elítéltek x                                                                 |")
+    print("|       d. A leprások                                                                     |")
+    print("-------------------------------------------------------------------------------------------")
+    print("             |                                          ")
+
+def q7():
+    print("-------------|-----------------------------------------------------------------------------")
+    print("|            Melyik megyében található a sok látogatót vonzó Bugacpuszta?                 |")
+    print("|       a. Csongrád                                                                       |")
+    print("|       b. Hajdú-Bihar                                                  (300.000Ft)       |")
+    print("|       c. Bács-Kiskun  x                                                                 |")
+    print("|       d. Pest                                                                           |")
+    print("-------------------------------------------------------------------------------------------")
+    print("             |                                          ")
+
+def q8():
+    print("-------------|-----------------------------------------------------------------------------")
+    print("|                       Ki a hair című musical zeneszerzője?                              |")
+    print("|       a. Galt McDermot x                                                                |")
+    print("|       b. Leonard Bernstein                                            (500.000Ft)       |")
+    print("|       c. Andrew Lioyd Webber                                                            |")
+    print("|       d. John Kander                                                                    |")
+    print("-------------------------------------------------------------------------------------------")
+    print("             |                                          ")
+
+def q9():
+    print("-------------|-----------------------------------------------------------------------------")
+    print("|          Ki volt Bill clintonrepublikánus ellenlábasa az 1996-os választásokon?         |")
+    print("|       a. Al Gore                                                                        |")
+    print("|       b. Ross Perot                                                   (800.000Ft)       |")
+    print("|       c. Robert Dolo x                                                                  |")
+    print("|       d. Michael Dukakis                                                                |")
+    print("-------------------------------------------------------------------------------------------")
+    print("             |                                          ")
+
+def q10():
+    print("-------------|-----------------------------------------------------------------------------")
+    print("|         Melyik nagy utazónk írta meg a török-tatár nyelvek etimológiai szótárat?        |")
+    print("|       a. Vámbéry Ármin x                                                                |")
+    print(f"|       b. Hopp Ferenc                                                  ({bcolors.WARNING}1.500.000Ft{bcolors.ENDC})     |")
+    print("|       c. Kőrösi Csoma Sándor                                                            |")
+    print("|       d. Germanus Gyula                                                                 |")
+    print("-------------------------------------------------------------------------------------------")
+    print("             |                                          ")
+
+def q11():
+    print("-------------|-----------------------------------------------------------------------------")
+    print("|                    Az alábbi űrahjósok közül ki nem járt a holdon?                      |")
+    print("|       a. David Scott                                                                    |")
+    print("|       b. James Irwin                                                (3.000.000Ft)       |")
+    print("|       c. Eugene A. Cernan                                                               |")
+    print("|       d. James Arthur Lowell x                                                          |")
+    print("-------------------------------------------------------------------------------------------")
+    print("             |                                          ")
+
+def q12():
+    print("-------------|-----------------------------------------------------------------------------")
+    print("|            Ki szokott harangozni, ha szegény az eklézsia a közmondás szerint?           |")
+    print("|       a. Maga a pap x                                                                   |")
+    print("|       b. A templom egere                                            (5.000.000Ft)       |")
+    print("|       c. A papné                                                                        |")
+    print("|       d. A szél                                                                         |")
+    print("-------------------------------------------------------------------------------------------")
+    print("             |                                          ")
+
+def q13():
+    print("-------------|-----------------------------------------------------------------------------")
+    print("|            Melyik folyó felett ível át a híres firenzei Ponte Vecchio híd?              |")
+    print("|       a. Adige                                                                          |")
+    print("|       b. Tevere                                                    (10.000.000Ft)       |")
+    print("|       c. Pó                                                                             |")
+    print("|       d. Arno x                                                                         |")
+    print("-------------------------------------------------------------------------------------------")
+    print("             |                                          ")
+
+def q14():
+    print("-------------|-----------------------------------------------------------------------------")
+    print("|            Melyik az a fa, melynek levelei élükkel fordulnak a nap felé?                |")
+    print("|       a. Majomkenyérfa                                                                  |")
+    print("|       b. Kaucsukfa                                                 (20.000.000Ft)       |")
+    print("|       c. Eukaliptuszfa x                                                                |")
+    print("|       d. Tiszafa                                                                        |")
+    print("-------------------------------------------------------------------------------------------")
+    print("             |                                          ")
+
+def q15():
+    print("-------------|-----------------------------------------------------------------------------")
+    print("|                    Milyen pitypangnak hívják a gyermekláncfüvet?                        |")
+    print("|       a. Patyolat                                                                       |")
+    print("|       b. Papucs                                                    (40.000.000Ft)       |")
+    print("|       c. Pongyola                                                                       |")
+    print("|       d. Parafa                                                                         |")
+    print("-------------------------------------------------------------------------------------------")
+    print("             |                                          ")
--- main.py
+++ main.py
@@ -1,3 +1,31 @@
-def main():
-    pass
+from time import sleep as wait
+from os import system as cmd
+from kerdesek import *
+penz = ""
+def k1():
+    q1()
+    startq = input("             ")
+    if startq == "c":
+        penz = 5000
+        k2()
+    else:
+        print(f"Nem jó válasz! Nyeremény: {penz}")
 
+
+def k2():
+    pass
+def main():
+    cmd("clear")
+    try:
+        start()
+        startq = input("             ")
+        if startq == "a":
+            k1()
+        else:
+            exit()
+    except ValueError:
+        main()
+
+
+if __name__ == "__main__":
+    main()
